130ecee6013b81f8ee89f03b437beda4243375d7
Leonard Rapp <leonardrapp@gmx.de>
2018-11-01 17:06:27 +0100

    serial and wifi setup

--- ledpp-esp32/ledpp-esp32.ino
+++ ledpp-esp32/ledpp-esp32.ino
@@ -1,7 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <WiFi.h>
+
+const char* ssid     = "ssid";
+const char* password = "passwordgit";
+
+void setup() {
+  // start serial connection
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  // start wifi connection
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+ 
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
